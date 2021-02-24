c9a4a5bf75c7b12308fa57825dff9cc84e5dc09f
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-02-24 21:42:39 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n){
+    let b = String(n);
+    let c = "";
+    for(let i=b.length-1; i>= 0; i--){
+        if(b[i] == "-"){
+        }else{
+            c += b[i];  
+        }    
+    }
+    return Number(c);
 }
